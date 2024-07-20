a2a69118460768cfd97be32d48d47132d57e7b25
NISHIMWE RUKUNDO Prosper <prosper.rk1@gmail.com>
2024-07-20 20:54:26 +0200

    ➕ routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,36 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import {Root} from "./routes/Root.jsx";
+import {Home} from "./routes/Home.jsx";
+import ErrorPage from "./routes/404Page.jsx";
+import WatchMovie from "./routes/WatchMovie.jsx";
+import {movieLoader} from "./loaders.js";
+
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Root />,
+        errorElement: <ErrorPage />,
+        children: [
+            {
+                path: "",
+                element: <Home />
+            },
+            {
+                path: "watch-movie/:movieId",
+                element: <WatchMovie />,
+                loader: movieLoader
+            }
+        ]
+    }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
