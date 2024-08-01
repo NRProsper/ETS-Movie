c96a5f0aa2af69076e9b423c6d2311cd49be2d2b
NISHIMWE RUKUNDO Prosper <prosper.rk1@gmail.com>
2024-08-01 18:11:33 +0200

    Dynamic routing in tendcard

--- src/components/TrendCard.jsx
+++ src/components/TrendCard.jsx
@@ -8,7 +8,7 @@ const TrendCard = ({movie, genres}) => {
 
 
     return (
-        <Link to={`watch-movie/${movie.id}`}
+        <Link to={!tv?`/watch-movie/${movie.id}`:`/watch-series/${movie.id}`}
              className="overflow-hidden"
         >
             <div className="h-[200px] lg:h-[341px] lg:w-full trend-card relative cursor-pointer">
