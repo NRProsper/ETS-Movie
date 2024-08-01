a995672dac049dd922db37be97396508968568fb
NISHIMWE RUKUNDO Prosper <prosper.rk1@gmail.com>
2024-08-01 16:26:05 +0200

    Added loader animation

---
+++ src/components/Loader.jsx
@@ -0,0 +1,13 @@
+
+
+export default function Loader() {
+    return (
+        <div className="loader-container">
+            <div className="loader">
+                <div className="loader-circle"></div>
+                <div className="loader-circle"></div>
+                <div className="loader-circle"></div>
+            </div>
+        </div>
+    )
+}
