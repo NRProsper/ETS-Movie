484aabb468b9ab679198022c2cb05796a2464bf0
NISHIMWE RUKUNDO Prosper <prosper.rk1@gmail.com>
2024-08-03 22:21:13 +0200

    finished login and signup forms

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -1,28 +1,10 @@
 import InputText from "../InputText.jsx";
 
 
-export default function SignUp() {
+export default function Login() {
     return(
         <div className="mt-6">
             <div>
-                <div className="grid grid-cols-2 gap-2">
-                    <InputText
-                        label={"First name"}
-                        type={"fname"}
-                        name={"fname"}
-                        id={"fname"}
-                        placeholder={"Enter your first name"}
-                        className={"mb-6"}
-                    />
-                    <InputText
-                        label={"Last name"}
-                        type={"lname"}
-                        name={"lname"}
-                        id={"lname"}
-                        placeholder={"Enter your last name"}
-                        className={"mb-6"}
-                    />
-                </div>
                 <InputText
                     label={"Email address"}
                     type={"email"}
@@ -43,7 +25,7 @@ export default function SignUp() {
             </div>
             <div className="flex items-center justify-end">
                 <button className="bg-[#E50000] font-semibold px-6 py-3 rounded-md">
-                    Sign up
+                    Login
                 </button>
             </div>
         </div>
--- src/routes/LoginSignUp.jsx
+++ src/routes/LoginSignUp.jsx
@@ -1,18 +1,47 @@
-import Login from "../components/Login.jsx";
+import SignUp from "../components/auth/SignUp.jsx";
+import Login from "../components/auth/Login.jsx";
+import {useState} from "react";
 
+const tabs = [
+    {
+        id: 1,
+        name: "Login"
+    },
+    {
+        id: 2,
+        name: "Sign up"
+    }
+]
 
 export default function LoginSignUp() {
+
+    const [currentTab, setCurrentTab] = useState(2)
+
     return(
         <div className="container mx-auto px-16 lg:px-32">
             <div className="pt-40 mx-72 ">
-                <div className="p-12 rounded-[12px] border-[#141414] border-2">
+                <div className="p-12 rounded-xl border-[#141414] border-2">
                     <div className="flex items-center justify-center">
-                        <div className="p-3 border-2 border-[#262626] mb-6 rounded-md inline-block">
-                            
+                        <div className="p-3 border-2 border-[#262626] mb-6 rounded-lg inline-block">
+                            <div className="flex items-center gap-5 font-medium">
+                                {
+                                    tabs.map((tab, idx) => (
+                                        <button
+                                            key={idx}
+                                            className={`px-4 py-2 ${currentTab === tab.id ? `rounded-md bg-[#E50000]`: ``}`}
+                                            onClick={() => setCurrentTab(tab.id)}
+                                        >
+                                            {tab.name}
+                                        </button>
+                                    ))
+                                }
+                            </div>
                         </div>
                     </div>
                     <div className="forms">
-                        <Login/>
+                        {
+                            currentTab === 1 ? <Login /> : <SignUp />
+                        }
                     </div>
                 </div>
             </div>
