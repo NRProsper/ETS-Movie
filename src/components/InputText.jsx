1f6f3455612813c21c2e8b60239c5f04d5ba8c54
NISHIMWE RUKUNDO Prosper <prosper.rk1@gmail.com>
2024-08-03 22:20:53 +0200

    added reusable input field

--- src/components/InputText.jsx
+++ src/components/InputText.jsx
@@ -1,12 +1,25 @@
+import PropTypes from "prop-types";
 
-export default function InputText() {
+export default function InputText({label, type, name, id, placeholder,className}) {
     return(
-        <input
-            type="text"
-            name=""
-            id=""
-            placeholder={"Enter you email"}
-            className="p-5 placeholder:text-[#999999] w-full rounded-[8px] bg-[#141414] border-2 border-[#262626]"
-        />
+        <label>
+            {label}
+            <input
+                type={type}
+                name={name}
+                id={id}
+                placeholder={placeholder}
+                className={`p-5 mt-2 placeholder:text-[#999999] w-full rounded-[8px] bg-[#141414] border-2 border-[#262626] ${className}`}
+            />
+        </label>
     )
+}
+
+InputText.prototype = {
+    label: PropTypes.string,
+    type: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    id: PropTypes.string.isRequired,
+    placeholder: PropTypes.string.isRequired,
+    className: PropTypes.string
 }
---
+++ src/components/auth/SignUp.jsx
@@ -0,0 +1,51 @@
+import InputText from "../InputText.jsx";
+
+
+export default function Login() {
+    return(
+        <div className="mt-6">
+            <div>
+                <div className="grid grid-cols-2 gap-2">
+                    <InputText
+                        label={"First name"}
+                        type={"fname"}
+                        name={"fname"}
+                        id={"fname"}
+                        placeholder={"Enter your first name"}
+                        className={"mb-6"}
+                    />
+                    <InputText
+                        label={"Last name"}
+                        type={"lname"}
+                        name={"lname"}
+                        id={"lname"}
+                        placeholder={"Enter your last name"}
+                        className={"mb-6"}
+                    />
+                </div>
+                <InputText
+                    label={"Email address"}
+                    type={"email"}
+                    name={"email"}
+                    id={"email"}
+                    placeholder={"Enter your email"}
+                    className={"mb-6"}
+                />
+
+                <InputText
+                    label={"Password"}
+                    type={"password"}
+                    name={"password"}
+                    id={"password"}
+                    placeholder={"Enter your password"}
+                    className={"mb-6"}
+                />
+            </div>
+            <div className="flex items-center justify-end">
+                <button className="bg-[#E50000] font-semibold px-6 py-3 rounded-md">
+                    Sign up
+                </button>
+            </div>
+        </div>
+    )
+}
