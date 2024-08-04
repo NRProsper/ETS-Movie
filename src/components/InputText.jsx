83d46ab898d57d358fc762dfc3487adb3db036ad
NISHIMWE RUKUNDO Prosper <prosper.rk1@gmail.com>
2024-08-04 19:02:20 +0200

    Added onChange event

--- src/components/InputText.jsx
+++ src/components/InputText.jsx
@@ -1,15 +1,15 @@
 import PropTypes from "prop-types";
 
-export default function InputText({label, type, name, id, placeholder,className}) {
+export default function InputText({label, type, id, placeholder,className, onChange}) {
     return(
         <label>
             {label}
             <input
                 type={type}
-                name={name}
                 id={id}
                 placeholder={placeholder}
                 className={`p-5 mt-2 placeholder:text-[#999999] w-full rounded-[8px] bg-[#141414] border-2 border-[#262626] ${className}`}
+                onChange={onChange}
             />
         </label>
     )
@@ -18,8 +18,8 @@ export default function InputText({label, type, name, id, placeholder,className}
 InputText.prototype = {
     label: PropTypes.string,
     type: PropTypes.string.isRequired,
-    name: PropTypes.string.isRequired,
     id: PropTypes.string.isRequired,
     placeholder: PropTypes.string.isRequired,
-    className: PropTypes.string
+    className: PropTypes.string,
+    onChange: PropTypes.func.isRequired
 }
